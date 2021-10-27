ab43c3d3d6ce4a606f1ee4d02c3b9ac563410db0
arthurtamm <arthurmtamm@gmail.com>
2021-10-27 12:15:26 -0300

    Modificando a função de distribuição de peças e iniciação do jogo

--- src/inicia_jogo.py
+++ src/inicia_jogo.py
@@ -1,12 +1,12 @@
-def inicia_jogo(j,pecas):
-    game = {}
-    game['jogadores'] = {}
-    game['monte'] = []
-    game['mesa'] = []
-    for i in range(0,j):
-        game['jogadores'][i] = []
-
-    for i in range(0,8):
-        game['jogadores'][i].append(pecas[i])
+def inicia_jogo(n, p):
+    game = {'jogadores': {}, 'monte': [], 'mesa': []}
+    k = 0
+    j = 7
+    for i in range(0, n):
+        game['jogadores'][i] = p[k:j]
+        k += 7
+        j += 7
+    game['monte'].extend(p[k:])
+    return game
             
             
