75931fce1ef3822eaab52d6f0308500be2b80f3a
arthurtamm <arthurmtamm@gmail.com>
2021-11-07 23:31:57 -0300

    Deletando arquivos não utilizados

--- src/soma_pecas.py
+++
@@ -1,7 +0,0 @@
-def soma_pecas(pecas):
-    soma = 0
-    if len(pecas) == 0:
-        return 0
-    for i in pecas:
-        soma += i[0] + i[1]
-    return soma
--- src/verifica_ganhador.py
+++
@@ -1,7 +0,0 @@
-def verifica_ganhador(hands):
-    for player, hand in hands.items():
-        if len(hand) == 0:
-            return player
-    return -1
-    
-    
